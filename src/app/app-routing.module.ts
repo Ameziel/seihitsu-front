32e73db49b64e65778e5dc105b30b773ac75d2d3
samuel <samuel.sicard.it@gmail.com>
2022-06-09 14:15:38 +0200

    Installation of the Angular's routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./public/accueil/accueil.component";
+import {ContactComponent} from "./public/contact/contact.component";
+import {ReservationComponent} from "./public/reservation/reservation.component";
+import {RestaurantComponent} from "./public/restaurant/restaurant.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AccueilComponent}, //TODO better way with the second line ? an OR ?
+  {path: 'Accueil', component: AccueilComponent},
+  {path: 'Chambre', component: AccueilComponent},
+  {path: 'Contact', component: ContactComponent},
+  {path: 'Reservation', component: ReservationComponent},
+  {path: 'Restaurant', component: RestaurantComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
