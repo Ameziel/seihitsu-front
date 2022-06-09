27d421e91eca4d3d18e69deae605057207afa0b4
samuel <samuel.sicard.it@gmail.com>
2022-06-09 15:27:57 +0200

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import {AccueilComponent} from "./public/accueil/accueil.component";
 import {ContactComponent} from "./public/contact/contact.component";
 import {ReservationComponent} from "./public/reservation/reservation.component";
 import {RestaurantComponent} from "./public/restaurant/restaurant.component";
+import {ChambreComponent} from "./public/chambre/chambre.component";
 
 const routes: Routes = [
   {path: '', component: AccueilComponent}, //TODO better way with the second line ? an OR ?
   {path: 'Accueil', component: AccueilComponent},
-  {path: 'Chambre', component: AccueilComponent},
+  {path: 'Chambre', component: ChambreComponent},
   {path: 'Contact', component: ContactComponent},
   {path: 'Reservation', component: ReservationComponent},
   {path: 'Restaurant', component: RestaurantComponent},
