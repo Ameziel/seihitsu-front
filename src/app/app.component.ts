1539b39493e0836cc56214d152c12724dff092bd
Cliel <samuel.sicard.it@gmail.com>
2022-06-15 13:41:13 +0200

    WIP have a conditional menu displayed by uer's statut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Seihitsu Hôtel";
+  public href: string = "";
+
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.href = this.router.url;
+  }
+
+  isSideNavigationVisible() {
+    return this.href.includes("interne");
+  }
 }
+
+
