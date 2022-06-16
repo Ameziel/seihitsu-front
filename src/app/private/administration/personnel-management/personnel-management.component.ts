dbf10e88ded4e0608b759ade98c69990e91bf867
Cliel <samuel.sicard.it@gmail.com>
2022-06-17 01:03:41 +0200

    Implement the way to get data back. WIP => Display properly into the component

--- src/app/private/administration/personnel-management/personnel-management.component.ts
+++ src/app/private/administration/personnel-management/personnel-management.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Employe} from "../../../models/employe";
+import {MatTableDataSource} from "@angular/material/table";
+import {HttpClient} from "@angular/common/http";
+
+
+
 
 @Component({
   selector: 'app-personnel-management',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personnel-management.component.scss']
 })
 export class PersonnelManagementComponent implements OnInit {
+  employes: Employe[] = [];
 
-  constructor() { }
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
 
+  getEmployes() {
+    this.httpClient.get<Employe[]>("http://localhost:8080/api/employe/all").subscribe( //make constant
+      reponse => {
+        this.employes = reponse;
+        console.log(this.employes);
+      }
+    )
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    // @ts-ignore
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
